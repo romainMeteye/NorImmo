24c3817c7c466d6498581d379ebf3b00193953c6
AxelDuval <axel.duval@gmail.com>
2021-10-14 17:26:32 +0200

    Modifications des fonctions liées au textarea message

--- js/contact.js
+++ js/contact.js
@@ -1,6 +1,7 @@
 const stringRegex = /^[a-zA-Z]+$/;
 const numberRegex = /^((\+)33|0)[1-9](\d{2}){4}$/;
-const blackList = ["sexe","sex","con","connard"]
+const blackList = ["sexe","sex","con","connard"];
+let insultPresence = false;
 
 // *** Prevent the form submit ***
 document.getElementById("submit").addEventListener("click", function(event){
@@ -15,8 +16,8 @@ document.getElementById("submit").addEventListener("click", function(event){
 
 //   const message = messageText.value;
   const counterShow = document.getElementById("maxLength");
-  const insultShow = document.getElementById("insult");
-  const maxLength = 10;
+  const insultShow = document.getElementById("messageError");
+  const maxLength = 20;
   let numberOfChars;
   counterShow.innerHTML = `Caractères restants : ${maxLength}`;
 
@@ -33,24 +34,53 @@ document.getElementById("submit").addEventListener("click", function(event){
     for (insult of blackList) {
         if (this.value.includes(insult)) {
         insultShow.innerHTML = `Un mot non autorisé a été détécté : "${insult}"`;
-        return true;
+        insultPresence = true;
+        this.classList.add('red');
+
         }
-    }
+        else {
+            insultPresence = false;
+        }
+        }
 };
   
 
-// *** Change input color when correct / incorrect for input type : text ***
-document.querySelectorAll('input[type="text"]').forEach((input) => {
+// *** Affichage des messages d'erreurs ***
+
+
+
+// *** Change input color when correct / incorrect for input type : lastName ***
+document.querySelectorAll('input[id="lastName"]').forEach((input) => {
     input.addEventListener('blur', function() {
         if (this.value.length < 2 || this.value.length > 50 || !this.value.match(stringRegex)){
             this.classList.remove('green');
             this.classList.add('red');
+            document.getElementById('lastNameError').textContent = "Le nom doit faire entre 2 et 50 caractères et ne doit contenir que des lettres";
       } else {       
         this.classList.remove('red');
-        this.classList.add('green');     
+        this.classList.add('green');
+        document.getElementById('lastNameError').textContent ="";     
       }
     });
   });
+
+
+// *** Change input color when correct / incorrect for input type : firstName ***
+document.querySelectorAll('input[id="firstName"]').forEach((input) => {
+    input.addEventListener('blur', function() {
+        if (this.value.length < 2 || this.value.length > 50 || !this.value.match(stringRegex)){
+            this.classList.remove('green');
+            this.classList.add('red');
+            document.getElementById('firstNameError').textContent = "Le prénom doit faire entre 2 et 50 caractères et ne doit contenir que des lettres";
+
+      } else {       
+        this.classList.remove('red');
+        this.classList.add('green');
+        document.getElementById('firstNameError').textContent = "";  
+      }
+    });
+  });
+
 
 // *** Change input color when correct / incorrect for input type : number ***
   document.querySelectorAll('input[type="number"]').forEach((input) => {
@@ -58,22 +88,26 @@ document.querySelectorAll('input[type="text"]').forEach((input) => {
         if (!this.value.match(numberRegex)){
             this.classList.remove('green');
             this.classList.add('red');
+            document.getElementById('phoneError').textContent = "Le numéro de téléphone n'est pas valide";
+
       } else {       
         this.classList.remove('red');
-        this.classList.add('green');     
+        this.classList.add('green');
+        document.getElementById('phoneError').textContent = "";   
       }
     });
   });
 
-  // *** Change input color when correct / incorrect for input type : message ***
-  document.querySelectorAll('#message').forEach((input) => {
-    input.addEventListener('blur', function() {
-        if (this.value.length <= 0 || this.value.length > maxLength || checkInsults){
-            this.classList.remove('green');
+// *** Change input color when correct / incorrect for : textarea ***
+const textArea = document.getElementById("message");
+textArea.addEventListener("input", checkTextArea);
+
+function checkTextArea() {
+    if (textArea.value.length <= 0 || textArea.value.length > maxLength || insultPresence){
+       this.classList.remove('green');
             this.classList.add('red');
-      } else {       
-        this.classList.remove('red');
-        this.classList.add('green');     
-      }
-    });
-  });
+        } else {       
+            this.classList.remove('red');
+            this.classList.add('green');
+              }
+  }
