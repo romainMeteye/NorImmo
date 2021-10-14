782263d3c26bd8504eb5b6de9eaf215c70134840
Anaïs <c7anais@gmail.com>
2021-10-14 09:51:27 +0200

    corrected result bug

--- js/Anaïs.js
+++ js/Anaïs.js
@@ -1,8 +1,9 @@
-document.getElementById("submitbtn").onclick = function() {
+
+document.getElementById("submitbtn").onclick = function(e) {
     let area = document.getElementById("area").value;
     let room = document.getElementById("room").value;
     let materials = document.getElementById("materials").value;
-    let estimate = document.getElementById("estimate");
- 
-    return estimate.innerText = (area*1000+room*10000)*materials+(area*1000+room*10000);
-}
+    e.preventDefault();
+    
+    return document.getElementById("estimate").innerText = (area*1000+room*10000)* materials + (area*1000+room*10000) + "€";  
+};
