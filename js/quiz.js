9699fd7aac130353709106abecbd01ad5c407334
harold Pierrache <h.pierrache@gmail.com>
2021-10-18 07:52:14 +0200

    quiz

---
+++ js/quiz.js
@@ -0,0 +1,24 @@
+let question = document.getElementById("question");
+let answer = document.getElementById("answer");
+let question_2 = document.getElementById("question_2");
+let answer_2 = document.getElementById("answer_2");
+let question_3 = document.getElementById("question_3");
+let answer_3 = document.getElementById("answer_3");
+
+question.onclick = function() {
+    answer.style.opacity = 1;
+}
+question_2.onclick = function() {
+    answer_2.style.opacity = 1;
+}
+question_3.onclick = function() {
+    answer_3.style.opacity = 1;
+}
+
+
+
+
+
+
+
+
